81267b3abfcfbd9d25771fef9e01704f44a95924
Spencer T. Parkin <Spencer.Parkin@L3T.com>
2022-09-24 12:26:29 -0600

    Fix comment.

--- Source/MeshTopology.h
+++ Source/MeshTopology.h
@@ -8,8 +8,8 @@ namespace MeshWarrior
 
 	// TODO: Write this class.  I'm not yet sure how to do it, but I have some ideas.
 	//       The result should be a useful description of the topology of a given mesh in
-	//       terms of all edges and holes of the mesh.  A line-loop (not necessarily a
-	//       polygon, convex or otherwise) could be used to represent all edges, and a
+	//       terms of all boundaries and holes of the mesh.  A line-loop (not necessarily a
+	//       polygon, convex or otherwise) could be used to represent all boundaries, and a
 	//       line-loop could also be used to represent the holes in smallest-circumference
 	//       form.  These line-loops should just be pointers into the mesh's vertex array.
 	//       Note that a Mobius Strip should be tested, as well as a Klein Bottle.
